bdcd1863b524dae1c7110d7696f41ae903fdde6d
Thomas Karp <karpthomas@gmail.com>
2024-07-01 17:51:10 +0200

    use free port on my server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 
